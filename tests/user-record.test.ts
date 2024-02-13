224508523c740450a4afd489e4b34ee93f2bc665
grzeg <grzegorzterenda@gmail.com>
2024-02-13 20:11:51 +0100

    Changed test flow, implemented DRY;

--- tests/user-record.test.ts
+++ tests/user-record.test.ts
@@ -1,8 +1,6 @@
 import {ObjectId} from "mongodb";
 import {UserEntity} from "../types";
 import {UserRecord} from "../records/user.record";
-import {isUsernameUnique} from "../utils/user.validation/user.validation";
-
 
 function createMockUser(): UserEntity {
     const random = Math.floor(Math.random() * 10000) + 1;
@@ -22,8 +20,12 @@ export async function insertMockUser(): Promise<string> {
     return UserRecord.insertUser(mockUser);
 }
 
+async function deleteMockUserFromDataBase(insertedId: string): Promise<void> {
+    await UserRecord.deleteUserById(insertedId);
+}
+
 //----------------------------------------------------------------
-test("Can build new UserRecord", async () => {
+it("Can build new UserRecord", async () => {
     const userRecordTest = createMockUser();
     expect(userRecordTest.username).toBe(userRecordTest.username);
     expect(userRecordTest.password).toBe(userRecordTest.password);
@@ -32,92 +34,90 @@ test("Can build new UserRecord", async () => {
     expect(userRecordTest.updatedAt).toBe(userRecordTest.updatedAt);
 });
 //----------------------------------------------------------------
-test("Can insert new user", async () => {
-    await UserRecord.deleteAllUsers();
-    const insertedUserId = await insertMockUser();
-    await expect(insertedUserId).toBeDefined();
-    await UserRecord.deleteUserById(insertedUserId);
+describe("Can insert and validate new user", () => {
+    it("Can insert new user", async () => {
+        const insertedUserId = await insertMockUser();
+        await expect(insertedUserId).toBeDefined();
+        await deleteMockUserFromDataBase(insertedUserId);
+    });
+
+    // USER VALIDATION
+    it("Invalid username throws an error", async () => {
+        const wrongUsernameMockUser = createMockUser();
+        wrongUsernameMockUser.username = "er";
+        const userRecord = new UserRecord(wrongUsernameMockUser);
+        const insertUser = async () => UserRecord.insertUser(userRecord);
+        await expect(insertUser).rejects.toThrowError();
+    });
+    it("Wrong password throws error", async () => {
+        const wrongPasswordMockUser = createMockUser();
+        wrongPasswordMockUser.password = "wrong";
+        const userRecord = new UserRecord(wrongPasswordMockUser);
+        const insertUser = async () => UserRecord.insertUser(userRecord);
+        await expect(insertUser).rejects.toThrowError();
+    });
+    it("Invalid email throws an error", async () => {
+        const wrongEmailMockUser = createMockUser();
+        wrongEmailMockUser.email = "wrongemail";
+        const userRecord = new UserRecord(wrongEmailMockUser);
+        const insertUser = async () => UserRecord.insertUser(userRecord);
+
+        await expect(insertUser).rejects.toThrowError();
+    });
+    it("Cannot insert user with taken username", async () => {
+        const mockUser = createMockUser();
+        const insertedUserId = await UserRecord.insertUser(mockUser);
+        try {
+            await UserRecord.insertUser(mockUser);
+        } catch (error) {
+            expect(error.message).toBe("Error: Invalid username: This username is already taken");
+        } finally {
+            await deleteMockUserFromDataBase(insertedUserId);
+        }
+    });
+    it("Cannot insert user with taken email", async () => {
+        const mockUser = createMockUser();
+        const insertedUserId = await UserRecord.insertUser(mockUser);
+        mockUser.username = `random${Math.floor(Math.random() * 10000)}${1}`;
+        mockUser._id = new ObjectId();
+        try {
+            await UserRecord.insertUser(mockUser);
+        } catch (error) {
+            expect(error.message).toBe("Error: Invalid email: This email is already taken.");
+        } finally {
+            await deleteMockUserFromDataBase(insertedUserId);
+        }
+    });
 });
-describe("Can delete all user records in userDb collection", () => {
-    it("should delete all", async () => {
-        await UserRecord.deleteAllUsers();
-        const foundedUser = await UserRecord.findAllUsers();
-        expect(foundedUser.length).toBe(0);
+
+//----------------------------------------------------------------
+describe("findAllUsers", () => {
+    it("should return an array of UserRecord objects when users are found", async () => {
+        expect(UserRecord.findAllUsers).toBeDefined();
+    });
+});
+
+
+//----------------------------------------------------------------
+
+describe("Find user by id returns User Entity or null", () => {
+    it("Can find single user record by _id", async () => {
+        const mockUser = createMockUser();
+        const insertedUserId = await UserRecord.insertUser(mockUser);
+        const user = await UserRecord.findUserById(insertedUserId);
+        await expect(user).toEqual(expect.objectContaining<UserEntity | null>(user));
+        if (user != null) {
+            await UserRecord.deleteUserById(insertedUserId);
+        }
+    })
+    it("Not founded user returns null", async () => {
+        const foundedUser = await UserRecord.findUserById("65b15b6973947f0159b8ad22");
+        await expect(foundedUser).toBeNull();
     });
 
 })
-//----------------------------------------------------------------
-test("Wrong password throws error", async () => {
-    const wrongPasswordMockUser = createMockUser();
-    wrongPasswordMockUser.password = "wrong";
-    const userRecord = new UserRecord(wrongPasswordMockUser);
-    const insertUser = async () => UserRecord.insertUser(userRecord);
-    await expect(insertUser).rejects.toThrowError();
-});
-//----------------------------------------------------------------
-test("Invalid username throws an error", async () => {
-    const wrongUsernameMockUser = createMockUser();
-    wrongUsernameMockUser.username = "er";
-    const userRecord = new UserRecord(wrongUsernameMockUser);
-    const insertUser = async () => UserRecord.insertUser(userRecord);
-    await expect(insertUser).rejects.toThrowError();
-});
-//----------------------------------------------------------------
-test("Invalid email throws an error", async () => {
-    const wrongEmailMockUser = createMockUser();
-    wrongEmailMockUser.email = "wrongemail";
-    const userRecord = new UserRecord(wrongEmailMockUser);
-    const insertUser = async () => UserRecord.insertUser(userRecord);
 
-    await expect(insertUser).rejects.toThrowError();
-});
 //----------------------------------------------------------------
-test("Can find single user record by _id", async () => {
-    const mockUser = createMockUser();
-    const insertedUserId = await UserRecord.insertUser(mockUser);
-    const user = await UserRecord.findUserById(insertedUserId);
-    await expect(user).toEqual(expect.objectContaining<UserEntity | null>(user));
-});
-//----------------------------------------------------------------
-test("Not founded user returns null", async () => {
-    const foundedUser = await UserRecord.findUserById("65b15b6973947f0159b8ad22");
-    await expect(foundedUser).toBeNull();
-});
-//----------------------------------------------------------------
-test("isUsernameUnique returns false if this username is already taken", async () => {
-    const insertedMockUser = await insertMockUser();
-    const insertedUser = await UserRecord.findUserById(insertedMockUser);
-    const result = await isUsernameUnique(insertedUser.username);
-    expect(result).toBeFalsy();
-});
-//----------------------------------------------------------------
-test("isUsernameUnique returns true if this username is unique", async () => {
-    const result = await isUsernameUnique("TestUniqueUser");
-    expect(result).toBeTruthy();
-});
-//----------------------------------------------------------------
-test("Cannot insert user with taken username", async () => {
-    const mockUser = createMockUser();
-    await UserRecord.insertUser(mockUser);
-    try {
-        await UserRecord.insertUser(mockUser);
-    } catch (error) {
-        expect(error.message).toBe("Error: Invalid username: This username is already taken");
-    }
-});
-//----------------------------------------------------------------
-test("Cannot insert user with taken email", async () => {
-    const mockUser = createMockUser();
-    await UserRecord.insertUser(mockUser);
-    mockUser.username = `random${Math.floor(Math.random() * 10000)}${1}`;
-    mockUser._id = new ObjectId();
-    try {
-        await UserRecord.insertUser(mockUser);
-
-    } catch (error) {
-        expect(error.message).toBe("This email is already taken! Try another one or try to recover password.");
-    }
-});
 describe("Can delete a user by given id", () => {
     it("should delete a user by given id", async () => {
         try {
@@ -140,8 +140,12 @@ describe("Can delete a user by given id", () => {
     });
 
 });
-describe("findAllUsers", () => {
-    it("should return an array of UserRecord objects when users are found", async () => {
-        expect(UserRecord.findAllUsers).toBeDefined();
+
+
+describe("Can delete all user records in userDb collection", () => {
+    it("should delete all", async () => {
+        await UserRecord.deleteAllUsers();
+        const foundedUser = await UserRecord.findAllUsers();
+        expect(foundedUser.length).toBe(0);
     });
 });
